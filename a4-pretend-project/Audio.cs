6dba9a84ecd264b50496edeb765ff3df4f68b2d4
Michael Burt <michael.burt@mohawkcollege.ca>
2024-11-14 19:42:26 -0500

    change some code in audio controller

--- a4-pretend-project/Audio.cs
+++ a4-pretend-project/Audio.cs
@@ -7,5 +7,13 @@ namespace Game10003
     {
 
         // my code will be here for the audio class
+
+        Vector2 position;
+        public void Setup()
+        {
+            float someNumber = 3.14159f;
+
+            position = new(50, 50);
+        }
     }
 }
