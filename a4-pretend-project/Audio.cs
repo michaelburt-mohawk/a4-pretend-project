a18a51f48d816eb3af6557aa3fd54963afa95880
Michael Burt <michael.burt@mohawkcollege.ca>
2024-11-14 19:40:42 -0500

    make more changes to audio controler
    ...

--- a4-pretend-project/Audio.cs
+++ a4-pretend-project/Audio.cs
@@ -7,5 +7,14 @@ namespace Game10003
     {
 
         // my code will be here for the audio class
+
+        public void Setup()
+        {
+            int a = 5;
+
+            Console.WriteLine("do some stuff...");
+
+            // this is code committed from mike-audio
+        }
     }
 }
